96b462794a32b7c3e5fd746e60eb3691a9e928b5
mbrynkier <mbrynkier@gmail.com>
2022-05-17 17:17:27 -0300

    PWA update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+const withPWA = require('next-pwa');
 
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable:false,
+  },
   reactStrictMode: true,
   images: {
     domains: ['api.lorem.space']
   }
-}
+})
+
 
